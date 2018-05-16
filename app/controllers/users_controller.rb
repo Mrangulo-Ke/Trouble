ce539a97a8874775941f2b7fd6bcfec252b769ae
mwangekapatrick1@gmail.com <mwangekapatrick1@gmail.com>
2018-05-16 17:16:53 +0300

    Add destroy other users by admin functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,22 +20,29 @@ class UsersController < ApplicationController
 	end
 	end
 
-	 def edit
+	def edit
 	 	
-	 end
+	end
 
-	 def update
+	def update
 	 	if @user.update(user_params)
 	 		flash[:success] = "Your account was updated successfully"
 	 		redirect_to articles_path
 	 	else
 	 		render 'edit'
 	 	end
-	 end
+	end
 
-	 def show
+	def show
 	 	@user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
-	 end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		flash[:danger] = "User and all articles created by user have been deleted"
+		redirect_to users_path
+	end
 
 	private
 	def user_params
